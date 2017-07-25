c5adfcda44f4c4e416127dbf2ccb79c225d3a63f
arendon094 <arendon094@gmail.com>
2017-07-24 22:26:56 -0700

    Set up app beginning. Modified html index to remove Google maps code

---
+++ src/containers/search_bar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component{
+  render(){
+    return(
+      <div>
+        Search bar and Intro will go here!
+      </div>
+    );
+  }
+}
