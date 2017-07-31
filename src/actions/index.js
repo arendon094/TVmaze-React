4fa20664cb28e7b16026020fa2abfc531104a317
arendon094 <arendon094@gmail.com>
2017-07-30 17:50:49 -0700

    Edited action and reducer for api call.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const ROOT_URL = 'http://api.tvmaze.com';
-
 export const FETCH_TVSHOWS = 'FETCH_TVSHOWS';
+export const FETCH_TVSHOW = 'FETCH_TVSHOW'
+
+const ROOT_URL = 'http://api.tvmaze.com';
 
 export function fetchTVShows(show){
   const url = `${ROOT_URL}/search/shows?q=${show}`;
@@ -10,6 +11,15 @@ export function fetchTVShows(show){
 
   return{
     type: FETCH_TVSHOWS,
+    payload: request
+  };
+}
+
+export function fetchTVShow(id){
+  const request = axios.get(`${ROOT_URL}/shows/${id}?embed=cast`);
+
+  return{
+    type: FETCH_TVSHOW,
     payload: request
   };
 }
--- src/reducers/reducer_shows.js
+++ src/reducers/reducer_shows.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { FETCH_TVSHOWS } from '../actions';
+import { FETCH_TVSHOWS, FETCH_TVSHOW } from '../actions';
 
 export default function(state = {}, action){
   switch (action.type) {
@@ -8,6 +8,8 @@ export default function(state = {}, action){
       return _.mapKeys(data, function(value, key){
         return data[key].show.id;
       });
+    case FETCH_TVSHOW:
+      return { ...state, [action.payload.data.show.id]: action.payload.data };
     default:
       return state;
   }
