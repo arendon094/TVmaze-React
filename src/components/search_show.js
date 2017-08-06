2a0fccc05ff9cb614610174572dec8a0854aa94f
arendon094 <arendon094@gmail.com>
2017-08-06 16:01:55 -0700

    Fixed navigation bar. Made it available to all components.

--- src/components/search_show.js
+++ src/components/search_show.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import SearchBar from '../containers/search_bar';
 import ShowsList from '../containers/shows_list';
-import AppNavbar from './app-navbar';
 
 export default class SearchShow extends Component {
   render() {
     return (
       <div>
-        <AppNavbar/>
         <div className="container">
           <SearchBar/>
           <ShowsList/>
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import SearchShow from './components/search_show';
 import ShowDetails from './containers/show-details';
+import AppNavbar from './components/app-navbar';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -15,6 +16,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
+        <AppNavbar/>
         <Switch>
           <Route path="/tvshow/:id" component={ShowDetails} />
           <Route path="/" component={SearchShow} />
